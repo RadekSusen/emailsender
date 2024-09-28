1a390c039c27b354a20b04d98f328a675dfb663b
Matyáš Flekač <flekac.matyas@seznam.cz>
2024-09-28 18:41:35 +0000

    Update App.java

--- app/src/main/java/utb/fai/App.java
+++ app/src/main/java/utb/fai/App.java
@@ -1,14 +1,36 @@
 package utb.fai;
 
-public class App {
-
+public class Main {
     public static void main(String[] args) {
-        // TODO: Implement input parameter processing
-        
+        // Ověření počtu argumentů
+        if (args.length != 6) {
+            System.out.println("Použití: java Main <SMTP server> <Port> <Odesílatel> <Příjemce> <Předmět> <Obsah>");
+            return;
+        }
+
+        // Získání parametrů z args
+        String smtpServer = args[0];  // Adresa SMTP serveru
+        int port = Integer.parseInt(args[1]);  // Port serveru
+        String from = args[2];  // Email odesílatele
+        String to = args[3];    // Email příjemce
+        String subject = args[4];  // Předmět emailu
+        String content = args[5];  // Obsah emailu
+
+        // Tvůj blok kódu s pevnými hodnotami
         try {
             EmailSender sender = new EmailSender("smtp.utb.cz", 25);
             sender.send("you@utb.cz", "you@utb.cz", "Email from Java", "Funguje to?\nSnad...");
             sender.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Kód s použitím argumentů z příkazové řádky
+        try {
+            EmailSender sender = new EmailSender(smtpServer, port);
+            sender.send(from, to, subject, content);
+            sender.close();  // Zavření spojení
+            System.out.println("Email byl úspěšně odeslán.");
         } catch (Exception e) {
             e.printStackTrace();
         }
