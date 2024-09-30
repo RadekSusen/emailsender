2296e11088eeb68274a6d9bc50c08a3de03ea9ad
Matěj Bača <m_baca@utb.cz>
2024-09-30 19:37:46 +0000

    Replace App.java

--- app/src/main/java/utb/fai/App.java
+++ app/src/main/java/utb/fai/App.java
@@ -4,10 +4,37 @@ public class App {
 
     public static void main(String[] args) {
         // TODO: Implement input parameter processing
+
+        String host = "";
+        int port = 0;
+        String from = "";
+        String to = "";
+        String subject = "";
+        String message = "";
+
+        if(args.length != 6) {
+            System.out.println("Usage: java -jar email-sender.jar <host> <port> <from> <to> <subject> <message>");
+            System.exit(1);
+        }
+
+        try {
+            host = args[0];
+            port = Integer.parseInt(args[1]);
+            from = args[2];
+            to = args[3];
+            subject = args[4];
+            message = args[5];
+
+        } catch (Exception e) {
+            System.out.println("Usage: java -jar email-sender.jar <host> <port> <from> <to> <subject> <message>");
+            System.exit(1);
+
+        }
+
         
         try {
-            EmailSender sender = new EmailSender("smtp.utb.cz", 25);
-            sender.send("you@utb.cz", "you@utb.cz", "Email from Java", "Funguje to?\nSnad...");
+            EmailSender sender = new EmailSender(host, port);
+            sender.send(from, to, subject, message);
             sender.close();
         } catch (Exception e) {
             e.printStackTrace();
