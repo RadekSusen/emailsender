773780c40224bfbdacb1bbfa9e216a069af1f10c
RadekSusen <95688472+RadekSusen@users.noreply.github.com>
2024-11-04 22:50:16 +0100

    Update App.java

--- app/src/main/java/utb/fai/App.java
+++ app/src/main/java/utb/fai/App.java
@@ -1,13 +1,19 @@
 package utb.fai;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class App {
 
     public static void main(String[] args) {
         // TODO: Implement input parameter processing
-        
+        System.out.println("Sending email...");
         try {
             EmailSender sender = new EmailSender("smtp.utb.cz", 25);
-            sender.send("you@utb.cz", "you@utb.cz", "Email from Java", "Funguje to?\nSnad...");
+            sender.send("r_susen@utb.cz", "r_susen@utb.cz", "Email from Java", "Funguje to?\nSnad...");
             sender.close();
         } catch (Exception e) {
             e.printStackTrace();
