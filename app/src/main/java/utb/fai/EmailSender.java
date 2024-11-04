9bc25532a4cdd2d479dc5df0fbabecdd9c19ebf9
RadekSusen <95688472+RadekSusen@users.noreply.github.com>
2024-11-04 22:50:33 +0100

    Update EmailSender.java

--- app/src/main/java/utb/fai/EmailSender.java
+++ app/src/main/java/utb/fai/EmailSender.java
@@ -10,7 +10,8 @@ public class EmailSender {
      * the exception is not handled in the constructor.
      */
     public EmailSender(String host, int port) throws UnknownHostException, IOException {
-
+        s = new Socket("smtp.utb.cz", 25);
+		out = s.getOutputStream();
     }
 
     /*
@@ -20,13 +21,25 @@ public class EmailSender {
      * handled by this method.
      */
     public void send(String from, String to, String subject, String text) throws IOException {
-
+                out.write(("HELO pc4-54-304\r\n"
+				+ "MAIL FROM: "+from+"\r\n"
+				+ "RCPT TO: "+to+"\r\n"
+				+ "DATA\r\n"
+				+ "Subject: "+subject+"\r\n"
+				+ text+"\r\n.\r\n").getBytes());
     }
 
     /*
      * Sends QUIT and closes the socket
      */
     public void close() {
-
+        out.write("QUIT\r\n".getBytes());
+		
+		InputStream inp = s.getInputStream();
+		int znak;
+		while((znak=inp.read())!=-1)
+			System.out.write(znak);
+		s.close();
+		
     }
 }
